e424df8247271e0f9d5f05902664354bcf5e4f46
maa94monti Monti <mario.monti@kellogg.northwestern.edu>
2023-06-03 22:21:23 +0000

    commiting

--- scripts/encryption.rb
+++
@@ -1,9 +0,0 @@
-
-# encrypt a secret
-secret = "tacos"
-encrypted_secret = BCrypt::Password.create(secret)
-puts encrypted_secret
-
-user_entered_secret = "tacos"
-result = BCrypt::Password.new(encrypted_secret) == user_entered_secret
-puts result
