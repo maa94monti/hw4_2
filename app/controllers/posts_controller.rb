d69be42ad2579ece1c0a7c52e608f2c9fa0ad9c0
maa94monti Monti <mario.monti@kellogg.northwestern.edu>
2023-06-03 23:50:13 +0000

    Adding navbar

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,13 +7,13 @@ class PostsController < ApplicationController
 
   def create
     if @current_user
-    @post = Post.new
-    @post["title"] = params["post"]["title"]
-    @post["description"] = params["post"]["description"]
-    @post["posted_on"] = params["post"]["posted_on"]
-    @post["place_id"] = params["post"]["place_id"]
-    @post["user_id"] = @current_user["id"]
-    @post.save
+      @post = Post.new
+      @post["title"] = params["post"]["title"]
+      @post["description"] = params["post"]["description"]
+      @post["posted_on"] = params["post"]["posted_on"]
+      @post["place_id"] = params["post"]["place_id"]
+      @post["user_id"] = @current_user["id"]
+      @post.save
     else
       flash["notice"] = "Login first."
     end
